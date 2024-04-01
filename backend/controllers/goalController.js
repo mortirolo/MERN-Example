66ad78e7474b5374ddf8d74ad0d17e02a71d9aee
mortirolo <roland.s@rogers.com>
2024-03-31 20:18:35 -0400

    Initial build of REST api (CRUD functionality) for goals model

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,4 +1,5 @@
 const asyncHandler = require('express-async-handler');  // Use express error handler to handle async-await statements instead of try-catch
+const Goal = require('../models/goalModel.js');
 
 // Prior to connecting to MongDB
 // const getGoals = (req, res) => {
@@ -12,7 +13,10 @@ const asyncHandler = require('express-async-handler');  // Use express error han
 // @route   GET /api/goals
 // @access  Private
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({message:'Get goals'})
+  const goals = await Goal.find()
+
+  // res.status(200).json({message:'Get goals'})  // wo/ DB
+  res.status(200).json(goals)
 })
 
 
@@ -21,29 +25,60 @@ const getGoals = asyncHandler(async (req, res) => {
 // @access  Private
 const setGoal = asyncHandler(async (req, res) => {
   // console.log(req.body)  // Test msg
+  // console.log(req.body.text)  // Test msg
+  // console.log('Checking for post error')
   if (!req.body.text) {
+    console.log('controller post error triggered: body text:', req.body.text)
     // If body has no text respond w/ status 400 and msg
     // res.status(400).json({message: 'Please add a text field'})
     res.status(400)
     // Use express error handler in ./middleware/errorMiddleware.js
+    // console.log('Throwing post error, controller')
     throw new Error('Please add a text field')  
-  } else {
-  res.status(200).json({message: 'Set goal'}) // specify that 
   }
+    const goal = await Goal.create({
+      text: req.body.text
+    })
+
+    //res.status(200).json({message: 'Set goal'}) // specify that 
+    res.status(200).json(goal) // specify that 
 })
 
 // @desc    Update goal
 // @route   PUT /api/goals/:id
 // @access  Private
 const updateGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({message: `Update goal ${req.params.id}`}) // specify that 
+  // Fetch goal by id
+  const goal = await Goal.findById(req.params.id)
+  // Make sure we get our goal db entry back
+  if(!goal) {
+    res.status(400)
+    throw new Error('Goal not found')
+  }
+
+  // Update the goal entry
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,  // This option creates DB entry if it doesn't already exist
+  })
+
+  res.status(200).json(updatedGoal) // specify that 
 })
 
 // @desc    Delete goal
 // @route   DELETE /api/goal/:id
 // @access  Private
 const deleteGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({message: `Delete goal ${req.params.id}`}) // specify that 
+  // Fetch goal by id
+  const goal = await Goal.findById(req.params.id)
+  // Make sure we get our goal db entry back
+  if(!goal) {
+    res.status(400)
+    throw new Error('Goal not found')
+  }
+
+  await goal.deleteOne()
+
+  res.status(200).json({ id: req.params.id })  // Return response and id for frontend use
 })
 
 module.exports = {
--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const goalSchema = mongoose.Schema({
   text: {
     type: String,
-    required: [true, 'Please add a text value']
+    required: [true, 'Please add a text value; (model schema)']
   }
 }, {
   timestamps: true,
